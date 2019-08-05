e57046ca73ceaf812fdac90b363538e8d7bcd05d
kumaruidivloper <kumaruidivloper@gmail.com>
2019-08-05 15:12:35 +0530

    window

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# BufferWhen
+# window
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.4.
 
-## Buffer When
-![004-Buffer-When](https://user-images.githubusercontent.com/30646609/62453826-c896a400-b790-11e9-9f95-1be86c90ba24.JPG)
+## Window
+![005-window](https://user-images.githubusercontent.com/30646609/62455056-2deb9480-b793-11e9-95b8-cc2100b5aea8.JPG)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { interval } from 'rxjs';
-import { take, tap, bufferWhen } from 'rxjs/operators';
+import { window, take, switchMap, toArray } from 'rxjs/operators';
 
 
 @Component({
@@ -11,26 +11,20 @@ import { take, tap, bufferWhen } from 'rxjs/operators';
 export class AppComponent {
   title = 'RxJS-Operators-by-Example-Playbook';
 
-  // bufferWhen
+  // Window
   
   constructor () {
-    // determine the closing point of a buffer
-    console.log('# vary buffer clearing based on value from interval');
-    let x = 0;
-    interval(500)
-      .pipe(
-          take(10),
-          tap(i => (x = i)),
-          bufferWhen(() => {
-              //vary buffer closing:
-              if (x < 5) {
-                return interval(1000);
-              }
-              return interval(500);
-          })
-      )
-      .subscribe(values => {
-        console.log(values);
-      });
-}
+    console.log('# emit the buffer after 1000 ms');
+    interval(100)
+        .pipe(
+            window(interval(1000)),
+            take(3), // <-- just to limit the life of the source Observable
+            // switchMap(w => w.pipe(toArry()))
+
+            switchMap(w => w.pipe(toArray()))
+    )
+    .subscribe(d => {
+        console.log(d);
+    });
+  }
 }
