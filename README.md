b05a0ced98157e90243dcaa49de1fbb9572ac871
kumaruidivloper <kumaruidivloper@gmail.com>
2019-08-05 13:41:49 +0530

    bufferCount

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# Buffer
+# BufferCount
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.4.
 
-## Operators Gonig to See
-![001-Operators-Gonig-to-See](https://user-images.githubusercontent.com/30646609/62447781-c7ab4580-b783-11e9-89ea-7204a3ca8515.JPG)
-
-## Use Case Of This Operators
-![002-Use-Case-Of-This-Operators](https://user-images.githubusercontent.com/30646609/62447782-c7ab4580-b783-11e9-85da-9d56d7a3bb8e.JPG)
-
-## Buffering & Windowing
-![003-Buffering   Windowing](https://user-images.githubusercontent.com/30646609/62447784-c7ab4580-b783-11e9-8194-4bb45f61305a.JPG)
-
-## Buffer
-![004-Buffer](https://user-images.githubusercontent.com/30646609/62447786-c843dc00-b783-11e9-8803-cd29adc4724d.JPG)
-
+## Buffer-Count
+![001-Buffer-Count](https://user-images.githubusercontent.com/30646609/62448894-718bd180-b786-11e9-902c-514956ead0ad.JPG)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { interval } from 'rxjs';
-import { buffer, take } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { bufferCount } from 'rxjs/operators';
 
 
 @Component({
@@ -10,27 +10,41 @@ import { buffer, take } from 'rxjs/operators';
 })
 export class AppComponent {
   title = 'RxJS-Operators-by-Example-Playbook';
-  
-  // buffer
-  // add vlaues into a buffer
-  // emit buffered values as array whenever
-  // inner observable emits
+
+  // bufferCount
+  // add value into buffer until full
+  // then emit the buffer
 
   constructor () {
-    console.log ('# emit the buffer after 1000 ms');
-    interval(100)
-      .pipe(
-          buffer(interval(1000)),
-          take(3) // <-- just to limit the life of the source Observable
-      )
-      .subscribe(d => {
-        console.log(d);
+    // #region example 1
+    console.log ('# emit buffer of 2 values or when complete');
+    of(1,2,3)
+      .pipe(bufferCount(2))
+      .subscribe(sequence => {
+          console.log(sequence);
       });
-  }
-  
-  // output
-  // [0, 1, 2, 3, 4, 5, 6, 7, 8]
-  // [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-  // [19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+    // Output:
+    // [1,2]
+    // [3]
+    // #endregion
 
-}
+
+
+    // #region example 2
+    console.log ();
+    console.log ('# emit buffer of 2 values');
+    console.log ('# start a new buffer when a new value emitted');
+    console.log ('# multiple buffers can coexist!');
+    
+    of(1,2,3)
+      .pipe(bufferCount(2,1))
+      .subscribe(sequence => {
+        console.log(sequence)
+      });
+
+    // Output:
+    // [1,2]
+    // [2,3]
+    // [3]
+    //#endregion
+}}
