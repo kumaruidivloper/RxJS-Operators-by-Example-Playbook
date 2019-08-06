956a0a1dbb64fcb80dbc272529f695acd3ee1df0
kumaruidivloper <kumaruidivloper@gmail.com>
2019-08-06 19:10:07 +0530

    retryWhen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { of } from 'rxjs';
-import { retry, map } from 'rxjs/operators';
+import { interval } from 'rxjs';
+import { retryWhen, map, scan, takeWhile, tap, retry } from 'rxjs/operators';
 
 
 @Component({
@@ -11,22 +11,54 @@ import { retry, map } from 'rxjs/operators';
 export class AppComponent {
   title = 'RxJS-Operators-by-Example-Playbook';
 
-  // retry
-  // resubscribe on error
+  // retryWhen
   
   constructor () {
-    console.log('# retry two times');
-    of('a', 1)
+    let swallowError = true;
+    interval(200)
         .pipe(
-            map(x => x.toUpperCase()),
-            retry(2)
+            map(x => {
+              console.log('attempting:' + x);
+              if(x=== 1 ) {
+                  throw 'error processing:' + x;
+              }
+              return x;
+            }),
+            retryWhen(errors => {
+                if (swallowError) {
+                  return errors.pipe(
+                    tap(err => console.log(err)),
+                    scan(acc => acc + 1, 0),
+                    tap(retryCount => {
+                      if(retryCount === 2) {
+                          console.log('Swallowing error and completing');
+                      } else {
+                          console.log('Retrying whole source - retry #' + retryCount)
+                      }
+                      return retryCount;
+                    }),
+                    takeWhile(errCount => errCount < 2)
+                  );
+                } else {
+                  return errors.pipe(
+                      tap(err => console.log(err)),
+                      scan(acc => acc + 1, 0),
+                      tap(retryCount => {
+                          if (retryCount === 2) {
+                              console.log('Failing');
+                              throw 'oops';
+                          } else {
+                              console.log('Retrying whole source - retry #' + retryCount)
+                          }
+                      })
+                  );
+                }
+            })
         )
-        .subscribe(x => console.log(x), e => console.log('error:', e.message));
-  }  
-
-  // OutPut
-  // A    <-- original attempt
-  // A    <-- retry #1
-  // A    <-- retry #2
-  // error: x.toUpperCase is not a function
+        .subscribe(
+              x => console.log('successfully processed:' + x),
+              err => console.log('**********error:' + err),
+              () => console.log('completed successfully')
+        );
+  }
 }
